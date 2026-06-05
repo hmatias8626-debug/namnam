7a75728f540f3bc55a91b3a1e5df6f7e29d226c8
hmatias8626-debug <hmatias8626@gmail.com>
2026-06-05 15:03:00 -0300

    color letra

--- services/ui.py
+++ services/ui.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 COLOR_DORADO = "#D89B1D"
-COLOR_NEGRO = "#111111"
 COLOR_CREMA = "#FFF7E6"
 
 
@@ -14,8 +13,37 @@ def apply_theme():
         color: #FFF7E6 !important;
     }}
 
-    html, body, [class*="css"] {{
+    h1, h2, h3, h4, h5, h6 {{
         color: #FFF7E6 !important;
+        opacity: 1 !important;
+        font-weight: 900 !important;
+    }}
+
+    p, label, small {{
+        color: #FFF7E6 !important;
+        opacity: 1 !important;
+        font-weight: 700 !important;
+    }}
+
+    div[data-testid="stMarkdownContainer"] p {{
+        color: #FFF7E6 !important;
+        opacity: 1 !important;
+    }}
+
+    div[data-testid="stCaptionContainer"] {{
+        color: #FFFFFF !important;
+        opacity: .95 !important;
+    }}
+
+    label,
+    .stTextInput label,
+    .stTextArea label,
+    .stSelectbox label,
+    .stNumberInput label,
+    .stRadio label {{
+        color: #FFF7E6 !important;
+        font-weight: 900 !important;
+        opacity: 1 !important;
     }}
 
     [data-testid="stSidebar"] {{
@@ -24,7 +52,7 @@ def apply_theme():
     }}
 
     [data-testid="stSidebar"] * {{
-        color: #ffffff !important;
+        color: #FFFFFF !important;
         opacity: 1 !important;
     }}
 
@@ -43,45 +71,6 @@ def apply_theme():
         margin-bottom: 7px !important;
     }}
 
-    [data-testid="stSidebar"] [role="radiogroup"] label:hover {{
-        background: rgba(216,155,29,.28) !important;
-        border: 1px solid {COLOR_DORADO} !important;
-    }}
-
-    h1, h2, h3, h4, h5, h6 {{
-        color: #FFF7E6 !important;
-        opacity: 1 !important;
-        font-weight: 900 !important;
-    }}
-
-    p, span, label, div, small {{
-        color: #FFF7E6 !important;
-        opacity: 1 !important;
-    }}
-
-    label,
-    .stTextInput label,
-    .stTextArea label,
-    .stSelectbox label,
-    .stNumberInput label,
-    .stRadio label,
-    .stCheckbox label {{
-        color: #FFF7E6 !important;
-        font-weight: 900 !important;
-        opacity: 1 !important;
-    }}
-
-    div[data-testid="stMarkdownContainer"] p {{
-        color: #FFF7E6 !important;
-        opacity: 1 !important;
-        font-weight: 600 !important;
-    }}
-
-    div[data-testid="stCaptionContainer"] {{
-        color: #ffffff !important;
-        opacity: .92 !important;
-    }}
-
     .nam-title {{
         color: {COLOR_DORADO} !important;
         font-weight: 950 !important;
@@ -97,7 +86,7 @@ def apply_theme():
     }}
 
     .metric-card small {{
-        color: #ffffff !important;
+        color: #FFFFFF !important;
         opacity: 1 !important;
         font-weight: 800;
     }}
@@ -107,21 +96,16 @@ def apply_theme():
         font-size: 28px;
     }}
 
-    div[data-testid="stMetricValue"] {{
-        color: #FFF7E6 !important;
-        opacity: 1 !important;
-        font-weight: 900 !important;
-    }}
-
+    div[data-testid="stMetricValue"],
     div[data-testid="stMetricLabel"] {{
-        color: #ffffff !important;
+        color: #FFF7E6 !important;
         opacity: 1 !important;
         font-weight: 900 !important;
     }}
 
     .stButton>button {{
         background-color: {COLOR_DORADO};
-        color: #111 !important;
+        color: #111111 !important;
         border-radius: 12px;
         border: 0;
         font-weight: 900;
@@ -129,23 +113,65 @@ def apply_theme():
 
     .stButton>button:hover {{
         background-color: #ffc247;
-        color: #111 !important;
+        color: #111111 !important;
         border: 0;
     }}
 
-    input, textarea, select {{
+    /* Inputs normales */
+    input, textarea {{
         color: #111111 !important;
+        background-color: #FFF7E6 !important;
         font-weight: 800 !important;
-        background-color: #fffaf0 !important;
     }}
 
+    /* Selectbox cerrado */
+    div[data-baseweb="select"] > div {{
+        background-color: #FFF7E6 !important;
+        color: #111111 !important;
+        border: 2px solid {COLOR_DORADO} !important;
+    }}
+
+    div[data-baseweb="select"] span {{
+        color: #111111 !important;
+        font-weight: 800 !important;
+    }}
+
+    /* Dropdown abierto del selectbox */
+    div[data-baseweb="popover"] * {{
+        color: #111111 !important;
+        background-color: #FFFFFF !important;
+        font-weight: 800 !important;
+    }}
+
+    div[data-baseweb="menu"] {{
+        background-color: #FFFFFF !important;
+    }}
+
+    div[data-baseweb="menu"] li,
+    div[data-baseweb="menu"] div {{
+        color: #111111 !important;
+        background-color: #FFFFFF !important;
+        font-weight: 800 !important;
+    }}
+
+    div[data-baseweb="menu"] li:hover,
+    div[data-baseweb="menu"] div:hover {{
+        background-color: #FFE3A1 !important;
+        color: #111111 !important;
+    }}
+
+    /* Number input botones */
+    button[kind="secondary"] {{
+        color: #111111 !important;
+    }}
+
+    /* Dataframes: fondo claro, texto oscuro */
     [data-testid="stDataFrame"] * {{
         color: #111111 !important;
     }}
 
     .stAlert * {{
-        color: inherit !important;
-        font-weight: 700 !important;
+        font-weight: 800 !important;
     }}
 
     div[role="radiogroup"] label {{
